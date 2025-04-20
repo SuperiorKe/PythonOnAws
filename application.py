ff45ae0b2c5675f4515c46fe7d65c826fc944910
superior <142397858+SuperiorKe@users.noreply.github.com>
2025-04-20 11:53:28 +0300

    Bug fixes

--- application.py
+++ application.py
@@ -1,10 +1,13 @@
+"""Module providing a function display text on '/'."""
 from flask import Flask
 
 application = Flask(__name__)
 
 @application.route('/')
 def hello_world():
+    """Function serving the text."""
     return 'Sup. Subscribe'
 
 if __name__ == '__main__':
     application.run(debug=True)
+    
